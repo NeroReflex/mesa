b83709f235392dd81c5b9ea61d6c1a7dc03cf8e9
Ruijing Dong <ruijing.dong@amd.com>
2023-12-08 18:18:01 -0500

    frontends/va: add ROI feature
    ROI (region of interest) feature implementation
    in va.
    
    It does not support ROI priority, and supports
    qp delta, and the maximum number of supported
    region is defined as 32, the region sequence implies
    the priority, the lower the sequence number, the higher
    the region priority, when region overlapping happened,
    the higher priority region overwrites the lower
    priority one.
    
    And specifically for AV1, the adjust step will be
    rounded by 5 when rate control is used, for example,
    if qp_delta (q index) is 6, it will use 5, if
    qp_delta is 8, it will use 10.
    
    For AVC/HEVC (RC/CQP) and AV1 CQP mode, the
    qp_delta granularity is 1.
    
    Reviewed-by: Boyuan Zhang <boyuan.zhang@amd.com>
    Signed-off-by: Ruijing Dong <ruijing.dong@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26659>

--- src/gallium/include/pipe/p_video_enums.h
+++ src/gallium/include/pipe/p_video_enums.h
@@ -152,6 +152,15 @@ enum pipe_video_cap
     * intra-refresh wave (e.g pipe_enc_intra_refresh.offset is 0 again)
    */
    PIPE_VIDEO_CAP_ENC_INTRA_REFRESH_MAX_DURATION = 46,
+   PIPE_VIDEO_CAP_ENC_H264_SUPPORTS_CABAC_ENCODE = 47,
+   /*
+      crop and partial decode support
+   */
+   PIPE_VIDEO_CAP_ROI_CROP_DEC = 48,
+   /*
+    * Encoding Region Of Interest feature
+    */
+   PIPE_VIDEO_CAP_ENC_ROI = 49,
 };
 
 enum pipe_video_h264_enc_dbk_filter_mode_flags
